99f3959ce482a9cc05192310190e699490c17638
Jordan Smith <jordansmith989@gmail.com>
2021-01-16 11:20:48 -0600

    maskSensitiveInfo

---
+++ maskSensitiveInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace maskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your secret");
+            var secret = Console.ReadLine();
+            var masker = secret.Substring(secret.Length - 4).PadLeft(secret.Length, '_');
+            Console.WriteLine(masker);
+        }
+    }
+}
